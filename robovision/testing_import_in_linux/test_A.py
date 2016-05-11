7831366b5ea541e8f37fc484f3a404fe6c983912
Microwave <vegetablejuiceftw@gmail.com>
2016-05-12 00:43:04 +0300

    fixed import issue in ubuntu, added example of problem in testing_imp...

---
+++ robovision/testing_import_in_linux/test_A.py
@@ -0,0 +1,10 @@
+import cv2
+from time import sleep
+import test_import_A
+
+for index in range(10):
+    temp_camera = cv2.VideoCapture(index)
+    sleep(0.05)
+    success, temp_frame = temp_camera.read()
+    print( 'Camera index {} is returned {}'.format( index,success) )
+   
